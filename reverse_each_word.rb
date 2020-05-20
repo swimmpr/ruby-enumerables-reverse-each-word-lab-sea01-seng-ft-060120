1675a5445bd410ca6b688fce67b7d4299faf725e
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-20 21:19:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,8 @@
-reverse_each_world(string)
+def reverse_each_word (string)
+    array_1 = string.split("")
+  array_2 = []
+  array_1.each do|string|
+    array_2 << string.reverse
+  end
+  array_2.join("")
+end
