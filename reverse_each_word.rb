e3dc112b1e15fa2b4e88e178c8158383dab180f6
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-20 21:20:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word (string)
   end
   array_2.join("")
 end
+
+def reverse_each_word (string)
+    array = string.split(" ")
+  new_array = []
+  array.collect do|string| 
+    new_array << string.reverse 
+  end
+  new_array.join(" ")
+end 
